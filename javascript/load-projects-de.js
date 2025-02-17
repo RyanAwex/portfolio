0929b6e71228278d4512fc0aeba5d61077f19313
Rayane Sefiani <ryanawex@gmail.com>
2025-02-17 20:40:02 +0100

    Add files via upload

---
+++ javascript/load-projects-de.js
@@ -0,0 +1,101 @@
+import {projects} from './projects.js';
+const showSidebar = document.querySelector('.menu-btn');  
+const closeSidebar = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+const themeToggleMain = document.getElementById("theme-toggle-main");
+const themeToggle = document.getElementById("theme-toggle");
+
+const lightModeMain = document.getElementById("light-mode-main");
+const darkModeMain = document.getElementById("dark-mode-main");
+const lightMode = document.getElementById("light-mode");
+const darkMode = document.getElementById("dark-mode");
+
+
+function saveToLocaleStorage() {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+themeToggleMain.addEventListener("click", () => {
+  saveToLocaleStorage();
+});
+
+themeToggle.addEventListener("click", () => {
+  saveToLocaleStorage();
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+darkModeMain.addEventListener('click', () => {
+  darkModeMain.style.display = 'none';
+  lightModeMain.style.display = 'flex';
+})
+
+lightModeMain.addEventListener('click', () => {
+  darkModeMain.style.display = 'flex';
+  lightModeMain.style.display = 'none';
+})
+
+
+
+darkMode.addEventListener('click', () => {
+  darkMode.style.display = 'none';
+  lightMode.style.display = 'flex';
+})
+lightMode.addEventListener('click', () => {
+  darkMode.style.display = 'flex';
+  lightMode.style.display = 'none';
+})
+
+
+// show Sidebar
+showSidebar.addEventListener('click', () => {
+  sidebar.style.display = 'flex';
+})
+  
+
+// close Sidebar
+closeSidebar.addEventListener('click', () => {
+  sidebar.style.display = 'none';
+})
+
+
+function loadProjectsDe() {
+  let projectsHTML = "";
+
+  projects.forEach((project) => {
+    projectsHTML += `
+      <section>
+        <div class="main-container">
+          <div class="image-container">
+            <img src="${project.image}" alt="project" />
+          </div>
+          <div class="description-container">
+            <div class="description">
+              <p class="en">
+                ${project.descriptionDE}
+              </p>  
+            </div>
+            <div class="button-container">
+              <a target="_blank" href="${project.buttonView}">
+                <button>View</button>
+              </a>
+              <a target="_blank" href="${project.buttonCode}">
+                <button>Code</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    `;
+  });
+  document.querySelector(".allProjects-de")
+    .innerHTML = projectsHTML;
+}
+loadProjectsDe();
---
+++ javascript/projects.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    image: `images/playtube-project.png`,
+    descriptionEn: `
+      This is a simple video platform with a modern design that's easy to navigate for discovering and watching popular videos. It features a clean and intuitive interface, tech reviews, and entertaining challenges. With clear view counts and upload dates, also developed using HTML, CSS, and JavaScript.
+    `,
+    descriptionDE: `
+      Dies ist eine einfache Videoplattform mit einem modernen Design, die leicht zu navigieren ist, um beliebte Videos zu entdecken und anzusehen. Sie verfügt über eine saubere und intuitive Benutzeroberfläche, Tech-Reviews und unterhaltsame Challenges. Mit klaren Aufrufzahlen und Upload-Daten, entwickelt mit HTML, CSS und JavaScript.
+    `,
+    buttonView: `https://playtube.rayanesefiani.com/`,
+    buttonCode: `https://github.com/RyanAwex/PlayTube`
+  }, {
+    image: `images/rps.jpg`,
+    descriptionEn: `
+      Developed using HTML, CSS, and JavaScript, this
+      Rock-Paper-Scissors game provides an interactive experience
+      with score tracking and an auto-play feature for automated
+      gameplay.
+    `,
+    descriptionDE: `
+      Entwickelt mit HTML, CSS und JavaScript bietet dieses
+      Schere-Stein-Papier-Spiel ein interaktives Erlebnis mit
+      Punktverfolgung und einer Auto-Play-Funktion für
+      automatisiertes Spielen.
+    `,
+    buttonView: `https://rps.rayanesefiani.com/`,
+    buttonCode: `https://github.com/RyanAwex/rock-paper-scissors-game`
+  },
+  {
+    image: `images/calculator-project.png`,
+    descriptionEn: `
+      This is a simple calculator app with a modern look that's easy
+      to use for everyday math like adding, subtracting,
+      multiplying, and dividing. It has a clean and intuitive
+      interface, making it a great tool for anyone who needs to
+      perform basic calculations quickly and easily.
+    `,
+    descriptionDE: `
+      Dies ist eine einfache Taschenrechner-App mit einem modernen
+      Aussehen, die sich leicht für alltägliche Mathematik wie
+      Addition, Subtraktion, Multiplikation und Division verwenden
+      lässt. Sie hat eine saubere und intuitive Benutzeroberfläche
+      und ist ein großartiges Werkzeug für jeden, der schnell und
+      einfach grundlegende Berechnungen durchführen muss.
+    `,
+    buttonView: `https://calculator.rayanesefiani.com/`,
+    buttonCode: `https://github.com/RyanAwex/calculator`
+  }
+];
